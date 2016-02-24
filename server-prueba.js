fc0060254a02408bcb9c251466f5560058fcdcbc
ralex <ralex.rdz@gmail.com>
2016-02-24 14:03:19 -0600

    server test 2

--- server-prueba.js
+++ server-prueba.js
@@ -5,11 +5,7 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
+server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
 
