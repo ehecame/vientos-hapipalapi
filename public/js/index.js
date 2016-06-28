aa2e7cc811ff15cef66a70c7bef79a10b2372473
ralex <ralex.rdz@gmail.com>
2016-06-28 14:16:04 -0600

    flip on

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
   $('#map-row').click(function (event) {
     window.location.href = '/map'
   })
+  $('.card').flip({trigger: 'manual', forceHeight: true})
+  $('.benefit').on('mouseover', function (event) {
+    console.log('over')
+    console.log($(this))
+    $(this).find('.card').flip(true)
+  })
+  $('.benefit').on('mouseout', function (event) {
+    console.log('LEAVE')
+    console.log($(this))
+    $(this).find('.card').flip(false)
+  })
 })
