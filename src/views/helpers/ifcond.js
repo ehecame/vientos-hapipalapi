032244adf6dadfcace46001606c21e5d04249704
ralex <ralex.rdz@gmail.com>
2016-07-22 18:12:56 -0600

    session expira en 30 min

--- src/views/helpers/ifcond.js
+++ src/views/helpers/ifcond.js
@@ -1,8 +1,4 @@
 var ifcond = function (v1, operator, v2, options) {
-  console.log(operator)
-  console.log(v1)
-  console.log(v2)
-  console.log(v1.indexOf(v2))
   switch (operator) {
     case '==':
       return (v1 == v2) ? options.fn(this) : options.inverse(this)
@@ -21,7 +17,7 @@ var ifcond = function (v1, operator, v2, options) {
     case '||':
       return (v1 || v2) ? options.fn(this) : options.inverse(this)
     case 'contains':
-      return (v1.indexOf(v2) != -1) ? options.fn(this) : options.inverse(this)
+      return (v1 && v1.indexOf(v2) != -1) ? options.fn(this) : options.inverse(this)
     default:
       return options.inverse(this)
   }
